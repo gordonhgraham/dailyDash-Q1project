f58e901c5a868c36a8de2d634e9b8b855ccb6577
Gordon H Graham <theboss@gordonhgraham.com>
2016-08-29 14:35:02 -0600

    update app.js with function to get and display time and date

--- app.js
+++ app.js
@@ -1,46 +1,65 @@
 $(document).ready(function() {
-    //
-    // function getDateTime() {
-    //     let today = new Date();
-    //     let min = today.getMinutes();
-    //     let hr = today.getHours();
-    //     let day = today.getDay();
-    //     let date = today.getDate();
-    //     let month = today.getMonth();
-    //     let year = today.getFullYear();
-    //   }
-    //
-    //   let displayDateTime = function() {
-    //     $('#date_time > h1').text();
-    //   }
-    //
-    // displayDateTime();
+  //get, format, display date and time
+  (function() {
+    let today = new Date()
 
+    function formatDay(i) {
+      const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+      return days[i];
+    }
+    //turn off if user selects 24 hour time display
+    function formatHour(h) {
+      return (h > 12) ? h - 12 : h;
+    }
 
-    (function() {
-        function formatTime(i) {
-            return (i < 10) ? "0" + i : i;
-        }
-        //turn off if user selects 24 hour time display
-        function formatHour(h) {
-          return (h > 12) ? h-12 : h;
-        }
+    function formatMonth(j) {
+      const months = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+      return months[j];
+    }
 
-        function getTime() {
-            var today = new Date(),
-                hr = formatHour(today.getHours()),
-                min = formatTime(today.getMinutes()),
-                sec = formatTime(today.getSeconds()); //time display breaks if seconds is removed. Needed for setTimeout?
-            $('#date_time > h1').text(hr + ":" + min)
-            t = setTimeout(function() {
-                getTime()
-            }, 500);
-        }
+    function formatTime(i) {
+      return (i < 10) ? "0" + i : i;
+    }
+
+    function getDate() {
+      let day = formatDay(today.getDay());
+      let date = today.getDate();
+      let month = formatMonth(today.getMonth());
+      let year = today.getFullYear();
+      $('#date').text(`${day} | ${month} ${date}, ${year}`);
+    }
+
+    function getTime() {
+      hr = formatHour(today.getHours()),
+        min = formatTime(today.getMinutes()),
+        sec = formatTime(today.getSeconds()); //time display breaks if seconds is removed. Needed for setTimeout?
+      $('#time').text(hr + ":" + min)
+      t = setTimeout(function() {
         getTime();
-    })();
+        getDate();
+      }, 500);
+    }
 
+    getDate();
+    getTime();
+  })();
 
-
+  //current weather
+//   (function() {
+//     console.log('Before AJAX');
+//     let $weather_data = $.getJSON('https://api.forecast.io/forecast/679f361d95ab79e1c8e2aa37494fa86d/40.055550,-105.208595');
+//     $weather_data.done(function(data) {
+//       if ($xhr.status !== 200) {
+//         return;
+//       }
+//       console.log('Hello');
+//       console.log(data['summary']);
+//       console.log(data['apparentTemperature']);
+//     });
+//     $weather_data.fail(function(err) {
+//     console.log(err);
+// });
+//   })();
 
 
 })
